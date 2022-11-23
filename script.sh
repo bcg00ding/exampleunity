4c7d1784985301c5972a8f8141982086f5d1ad09
Todd Carr <toddc@unity3d.com>
2022-11-23 12:09:47 -0600

    Updates to script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,16 @@
+#!/usr/bin/env bash
+
+# Logic
+
+## Source profile
+. ~/.profile
+
+## Install NVM modules and set version
 nvm list
 nvm install 8.12.0
 nvm use 8.12.0
 
+## Output Node version used
 node -v
 
 exit 0

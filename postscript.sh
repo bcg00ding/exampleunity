9f510e30333e2b63570c1bf4eaccb04171db6e21
Benjamin <benjamin.gooding@unity3d.com>
2023-01-27 13:13:53 -0600

    Try windows paths

--- postscript.sh
+++ postscript.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 whoami
-ls -lrt '$(cygpath -w "/cygdrive/c/Users/buildbot/Application Data/Microsoft/Crypto/RSA/*")'
-ls -lrt '$(cygpath -w "/cygdrive/c/Users/buildbot/Application Data/Microsoft/Crypto/RSA/S-1-5-21-3446310194-4090932778-461605731-1001")'
+ls -lrt "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA"
+ls -lrt "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA\\S-1-5-21-3446310194-4090932778-461605731-1001\\"
 
-touch '$(cygpath -w "/cygdrive/c/Users/buildbot/Application Data/Microsoft/Crypto/RSA/S-1-5-21-3446310194-4090932778-461605731-1001/temp.txt")'
+touch "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA\\S-1-5-21-3446310194-4090932778-461605731-1001\\temp.txt"
 
-ls -lrt '$(cygpath -w "/cygdrive/c/Users/buildbot/Application Data/Microsoft/Crypto/RSA/S-1-5-21-3446310194-4090932778-461605731-1001")'
+ls -lrt "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA\\S-1-5-21-3446310194-4090932778-461605731-1001\\"
 
-echo "IS ATTY? ${TERM}"
+echo "IS ATTY? $TERM"
 
 sleep 300000000

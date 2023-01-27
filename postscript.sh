b8daa1a26c93531e60736b2f3b1bd61873e58c7b
Benjamin <benjamin.gooding@unity3d.com>
2023-01-27 12:55:43 -0600

    Post script and sleep

--- postscript.sh
+++ postscript.sh
@@ -9,3 +9,5 @@ touch '$(cygpath -w "/cygdrive/c/Users/buildbot/Application Data/Microsoft/Crypt
 ls -lrt '$(cygpath -w "/cygdrive/c/Users/buildbot/Application Data/Microsoft/Crypto/RSA/S-1-5-21-3446310194-4090932778-461605731-1001")'
 
 echo "IS ATTY? ${TERM}"
+
+sleep 300000000

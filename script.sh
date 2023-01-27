252d0e50f438c66b378a504445977efe1d8e5b07
Benjamin <benjamin.gooding@unity3d.com>
2023-01-27 13:39:29 -0600

    Sleep in pre-build

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 whoami
-ls -lrt "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA"
+ls -lrt "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA\\*"
 ls -lrt "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA\\S-1-5-21-3446310194-4090932778-461605731-1001\\"
 
 touch "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA\\S-1-5-21-3446310194-4090932778-461605731-1001\\temp.txt"
 
-ls -lrt "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA\\S-1-5-21-3446310194-4090932778-461605731-1001\\"
+ls -lrt "C:\\Users\\buildbot\\Application Data\\Microsoft\\Crypto\\RSA\\S-1-5-21-3446310194-4090932778-461605731-1001\\*"
 
-echo "IS ATTY? $TERM"
+
+sleep 300000000

ed24e0733b145f5915f810a5252fddb8947472ed
Benjamin <benjamin.gooding@unity3d.com>
2023-06-02 10:51:01 -0500

    secret masking

--- script.sh
+++ script.sh
@@ -6,3 +6,7 @@ echo "invalid" >> $DEVOPS_ENV
 echo "invalid2=" >> $DEVOPS_ENV
 
 cat $DEVOPS_ENV
+
+echo "::mask-env::VARIABLE_NAME"
+echo "::mask-value::SOME_VALUE_TO_HIDE"
+echo "There should be stars in place of SOME_VALUE_TO_HIDE and $VARIABLE_NAME"
